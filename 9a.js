aae3dfebca2f0740331015e02ec6c2c34947f372
shivam <shivamtiwarisgbt@gmail.com>
2025-03-24 11:36:09 +0530

    yea

---
+++ 9a.js
@@ -0,0 +1,4 @@
+document.querySelector('.js-button');
+
+  const button = document.querySelector('.js-button-2nd').innerHTML = '9b done!';
+    console.log(button);
---
+++ 9c.js
@@ -0,0 +1,6 @@
+function onclickHeads() {
+    document.querySelector('.paragraph').innerHTML = 'You choose: heads'
+}
+function onclickTails(params) {
+    document.querySelector('.paragraph').innerHTML = 'You choose: tails'
+}
