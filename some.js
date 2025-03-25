12172be7b0fe7a7decad3dafc61b3d8ec05922c8
shivam <shivamtiwarisgbt@gmail.com>
2025-03-25 16:50:08 +0530

    hgh

---
+++ some.js
@@ -0,0 +1,11 @@
+const nums =[1, 1, 3];
+
+const numsDoubled = [];
+
+for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    numsDoubled.push(num * 2);
+}
+
+console.log(numsDoubled);
+    
