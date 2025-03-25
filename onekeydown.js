f77cdd975a483c40113ecd56c4150e657adef266
SHIVAM TIWARI <142523464+Shivamisrich@users.noreply.github.com>
2025-03-25 08:37:17 +0530

    Update onekeydown.js

--- onekeydown.js
+++ onekeydown.js
@@ -1,7 +1,7 @@
  const inputBox = document.querySelector('.js-input-button');
  const displayText = document.querySelector('.js-display-text');
  //the above 2 lines are getting the input box and the display text
- //now explain the below line from basic level
+
 
  inputBox.onkeydown = function(event) {
     displayText.innerHTML = event.key;
