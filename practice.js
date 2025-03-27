e0baac080dc2117bfef98eb3f551d8e0d045d13f
shivam <shivamtiwarisgbt@gmail.com>
2025-03-27 08:48:09 +0530

    Todolist

--- practice.js
+++ practice.js
@@ -1,26 +1,81 @@
 const todoList = [];
-// renderTodoList();
 
-function renderTodoList(params) {
 
-let innerHTML = '';
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
 
-for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i]; 
-    const html = `<p>${todo}</p>`;
-    innerHTML = innerHTML + html;
-    
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+    body.setAttribute('data-theme', 'dark');
+    darkModeToggle.checked = true;
 }
-document.querySelector('.js-todo-list').innerHTML = innerHTML;
 
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        body.removeAttribute('data-theme');
+        localStorage.setItem('darkMode', null);
+    }
+});
+
+
+renderTodoList();
+
+function renderTodoList() {
+    let innerHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i]; 
+        const name = todoObject.name;
+        const duedate = todoObject.duedate;
+        
+        const html = `
+        <div class="todo-item" id="todo-${i}">
+            <div class="todo-content">
+                <div>${name}</div>
+                <div class="todo-date">Due: ${duedate}</div>
+            </div>
+            <button onclick="deleteTodo(${i})" class="delete-todo-button">Delete</button> 
+        </div>
+        `;
+        innerHTML = innerHTML + html;
+    }
+    document.querySelector('.js-todo-list').innerHTML = innerHTML;
+}
+
+function deleteTodo(index) {
+    const todoElement = document.getElementById(`todo-${index}`);
+    
+  
+    todoElement.classList.add('deleting');
+    
+    
+    setTimeout(() => {
+        todoList.splice(index, 1);
+        renderTodoList();
+    }, 300); 
 }
 
 function addTodo() {
-   const inputElement = document.querySelector('.js-name-input')
-   const name = inputElement.value;
-   todoList.push(name);
-   console.log(todoList);
-   renderTodoList();
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    const dateInputElement = document.querySelector('.js-date-input');
+    const duedate = dateInputElement.value;
+    
+    if (name.trim() !== '') {
+        todoList.push({
+            name: name,
+            duedate: duedate
+        });
+        
+        
+        inputElement.value = '';
+        dateInputElement.value = '';
+        
+        renderTodoList();
+    }
 }
    
 
