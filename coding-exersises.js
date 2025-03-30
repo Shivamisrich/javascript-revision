c42eef6f93ed0f0e52022e9a7dad589704217164
shivam <shivamtiwarisgbt@gmail.com>
2025-03-30 08:06:53 +0530

    dcdc

---
+++ coding-exersises.js
@@ -0,0 +1,141 @@
+// 1. swap array
+/*
+function arraySwap(array){
+    let temp = ''; // temporary variable bana diye hai.
+   temp  = array[0]; // this assign the value of 1st element of the array to the temp.
+   array[0] = array[array.length-1];// this assign the value of last element to the 1st place.
+   array[array.length-1] = temp; // this assign the value of temp to the last place.
+
+    return array;
+}
+console.log(arraySwap([1, 2 , 3 , 4]));
+*/
+
+
+// 2. loop for and while 
+/*
+for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+    
+}
+let i = 0;
+while (i <=10) {
+    i+=2;
+    console.log(i);
+}
+
+*/
+// let i = 6;
+// while (i>0) {
+//     i = i-1;
+//     console.log(i);
+// }
+// function increaseByOne(array) {
+//     const newArray = []; // Declare inside the function
+//     for (let i = 0; i < array.length; i++) {
+//         newArray.push(array[i] + 1); // Directly push incremented value
+//     }
+//     return newArray; // Return instead of console.log
+// }
+
+// console.log(increaseByOne([1, 2, 3, 4]));
+
+// function addNum(array,num) {
+//    let newArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//       newArray.push(array[i] + num);        
+//     }
+//     return newArray;
+// }
+// console.log(addNum([1,2,3,4],5));
+
+// function addArrays(array1,array2) {
+//     let array3 = [];
+//      for (let i = 0; i < array1.length; i++) {
+//             array3.push(array1[i]+array2[i]);        
+//      }
+//     return array3;
+// }
+// console.log(addArrays([8,9,97],[8, 9,10]));
+
+// function addArrays(array1,array2) {
+//     let result = [];
+//     let maxLimit =  Math.max(array1.length,array2.length);
+//    for (let i = 0; i < maxLimit; i++) {
+//     let num1 = array1[i] || 0;
+//     let num2 = array2[i] || 0;
+//     result.push(num1+num2);
+    
+    
+//    }
+//    return result;
+// }
+//  console.log(addArrays([8,9,97],[8,10]));
+// function countPositive(nums) {
+//     let count = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] < 0) {
+//             count++;
+//         }
+        
+//     }
+//     return count;
+// }
+// console.log(countPositive([-1,22,0,-999]));
+
+// function minMax(nums) {
+    
+//     let min = nums[0];
+//     let max = nums[0];
+    
+//     for (let i = 1; i < nums.length; i++) {
+        
+//         if (nums[i] > max) {
+//             max = nums[i];
+//         }
+//          if (nums[i] < min) {
+//             min = nums[i];
+//         }
+        
+     
+    
+//     //return object1;
+//     return  {
+//         minimum: min,
+//         maximum: max
+
+//     };
+
+
+// }
+
+
+// }
+// console.log(minMax([1,22,44,22,543]));
+
+
+// function countWords(words) {
+//     let Apple = 0;
+//     let Grape = 0;
+//     for (let i = 0; i < words.length; i++) {
+//         const element = words[i];
+//         if(element === 'apple'){
+//             Apple++;
+//         }
+//         if (element === 'grape') {
+//             Grape++;
+//         }
+        
+//     }
+//     return {
+//         apple: Apple,
+//         grape: Grape
+//     };
+// }
+// console.log(countWords(['apple','apple','grape','apple']));
+
+
+
+
+
+
