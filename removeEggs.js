a13b053092784ad3e991fdcb32289da04647f95f
shivam <shivamtiwarisgbt@gmail.com>
2025-03-31 19:57:07 +0530

    yea

--- removeEggs.js
+++ removeEggs.js
@@ -12,17 +12,39 @@
 // console.log(removeEggs(['eggs','burger','hams']));
 
 
-function removeEggs(foods) {
-    let count = 0;
-    let newArray = [];
-    for (let i = 0; i < foods.length; i++) {
-       if(foods[i]==='eggs'){ 
-        count++;
+// function removeEggs(foods) {
+//     let count = 0;
+//     let newArray = [];
+//     for (let i = 0; i < foods.length; i++) {
+//        if(foods[i]==='eggs'){ 
+//         count++;
         
-       }
-    }
-    return newArray;
-}
-console.log(removeEggs(['eggs','burger','hams']));
+//        }
 
+//     }
+//     return newArray;
+// }
+// console.log(removeEggs(['eggs','burger','hams']));
+// function removeEggs(params) {
+    
+// }
+
+function removeEggs(foods) {
+    foods.reverse();
+    let newarray  = [];
+    let count = 0;
+   for (let i = 0; i < foods.length; i++) {
+    if (count < 2 && foods[i]==='eggs') {
+        count++; 
+        continue;           //
+
+    }
+    else{
+        newarray.push(foods[i]);
+    }
+    
+   }
+   return newarray.reverse();
+}
+console.log(removeEggs(['eggs','hams','burger','eggs','eggs']));
 
