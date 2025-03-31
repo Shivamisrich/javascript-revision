cba4f8acac989636ee4363612e12590996328fd9
shivam <shivamtiwarisgbt@gmail.com>
2025-03-31 19:02:57 +0530

    bjjbj

--- removeEggs.js
+++ removeEggs.js
@@ -1,3 +1,28 @@
+// function removeEggs(foods) {
+//     let newArray = [];
+//     for (let i = 0; i < foods.length; i++) {
+//         foods[i];
+//         if (foods[i]!='eggs') {
+//             newArray.push(foods[i]);
+//         }
+        
+//     }
+//     return newArray;
+// }
+// console.log(removeEggs(['eggs','burger','hams']));
+
+
 function removeEggs(foods) {
-    
+    let count = 0;
+    let newArray = [];
+    for (let i = 0; i < foods.length; i++) {
+       if(foods[i]==='eggs'){ 
+        count++;
+        
+       }
+    }
+    return newArray;
 }
+console.log(removeEggs(['eggs','burger','hams']));
+
+
