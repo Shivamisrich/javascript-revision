694c08c1ae2013dcd7bbb924fa7f415c2de17a26
shivam <shivamtiwarisgbt@gmail.com>
2025-04-08 08:07:40 +0530

    fizzBuzz

--- script.js
+++ script.js
@@ -1,40 +1,18 @@
-const sentences = [
-    "The quick brown fox jumps over the lazy dog",
-    "Typing fast is a great skill",
-    "JavaScript is a powerful language",
-    "Practice makes a man perfect",
-    "I love solving problems with code"
-  ];
-  
-  let currentSentence = "";
-  let startTime;
-  
-  function startTest() {
-    // Reset everything
-    const randomIndex = Math.floor(Math.random() * sentences.length);
-    currentSentence = sentences[randomIndex];
-    document.getElementById("sentence").innerText = currentSentence;
-    document.getElementById("input").value = "";
-    document.getElementById("result").innerText = "";
-    startTime = null;
-  }
-  
-  document.getElementById("input").addEventListener("input", function () {
-    const typedText = this.value;
-  
-    if (!startTime) {
-      startTime = new Date();
+//FizzBuzz problem.
+for (let i = 1; i <= 20; i++) {
+   
+    if (i%3===0 && i%5===0) {
+        console.log('FizzBuzz');
     }
-  
-    if (typedText === currentSentence) {
-      const endTime = new Date();
-      const timeTaken = (endTime - startTime) / 1000; // in seconds
-      const wordCount = currentSentence.split(" ").length;
-      const speed = Math.round((wordCount / timeTaken) * 60);
-      document.getElementById("result").innerText =
-        `✅ Completed in ${timeTaken.toFixed(2)} seconds\n⚡ Speed: ${speed} WPM`;
+   else if(i%3===0){
+        console.log('Fizz');
     }
-  });
-  
-  // Auto start on load
-  startTest();
+    else if (i%5===0) {
+        console.log('Buzz');
+    }
+    else{
+        console.log(i);
+    }
+    
+    
+}
